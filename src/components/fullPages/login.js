72ccacec501c9048c98adfe207ff85f4e8af89bf
aronedwards91 <aronedwards91@gmail.com>
2018-09-20 10:46:30 +0100

    login now looks good, logic in backend now required

--- src/components/fullPages/login.js
+++ src/components/fullPages/login.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
+import {Button, Form, FormControl, FormGroup, Table, ControlLabel} from 'react-bootstrap';
+import axios from 'axios';
+import BASE_URL from '../constants'
 
 export default class Login extends Component{
+    searchUser() {
+        //TODO: axios setup
+        document.getElementById("username");
+    }
+
     render(){
+        var padding = {
+            padding: 40,
+            maxWidth: 500
+        }
+
+        var minPadding = {
+            padding: 15
+        }
         return(
-            <div>
-                <form action="/login">
-                    <input type="text" name="name" placeholder="Enter name" />
-                    <input type="text" name="password" placeholder="Enter Password" />
-                    <input type="submit" value="Submit" />
-                </form>
+            <div style={padding}>
+                <Form action="/login" method="post">
+                    <ControlLabel>Username:</ControlLabel>
+                    <FormControl
+                        id="username"
+                        type="text"
+                    />
+                    <ControlLabel>Password:</ControlLabel>
+                    <FormControl
+                        id="password"
+                        type="text"
+                    />
+                    <br></br>
+                    <Button style={minPadding} type="submit">Log in</Button>
+                </Form>
             </div>
         );
     }
